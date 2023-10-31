a351002d92e25bcead1b7fdf08df5443f2554bcc
Feast <feast107@gmail.com>
2023-10-31 09:11:21 +0800

    Force case naming

---
+++ src/Antelcat.Wpf.I18N/Attributes/ResourceKeysOfAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Antelcat.Wpf.I18N.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class ResourceKeysOfAttribute : Attribute
+{
+    public ResourceKeysOfAttribute(Type resourceType) { }
+}
