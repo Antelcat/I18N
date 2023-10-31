82d878fcfc58a4407b08382e1f7cfe3764ac6719
Feast <feast107@gmail.com>
2023-10-31 09:14:21 +0800

    move

--- src/Antelcat.Wpf.I18n/Attributes/ResourceKeysOfAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Antelcat.Wpf.I18N.Attributes;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = false)]
-public class ResourceKeysOfAttribute : Attribute
-{
-    public ResourceKeysOfAttribute(Type resourceType) { }
-}
