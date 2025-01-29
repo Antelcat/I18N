73e433c3c591f7d821232178a3ecaf6ebeb03d81
Feast <feast107@gmail.com>
2025-01-29 23:03:39 +0800

    Add DisableGenerationForAttribute

---
+++ src/Antelcat.I18N.Abstractions.Shared/Attributes/DisableGenerationForAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Antelcat.I18N.Attributes;
+
+/// <summary>
+/// attributes excluded in generation
+/// </summary>
+/// <param name="attributeTypes"></param>
+[AttributeUsage(AttributeTargets.Assembly)]
+public class DisableGenerationForAttribute(params Type[] attributeTypes) : Attribute;
